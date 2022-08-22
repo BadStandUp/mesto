7794258d27929f4818dbbb586e7f079ab5367b22
BadStandUp <sackboysema@yandex.ru>
2022-08-22 21:31:07 +0500

    fix: bugfix after review

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,25 @@
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button');
-let popup = document.querySelector('.popup')
-let likeButton = document.querySelector('.element__like-button');
-let saveButton = document.querySelector('.popup__save-button');
-
-function toggleLike () {
-  likeButton.classList.toggle('element__like-button_active');
-}
-
-function togglePopup () {
-  popup.classList.toggle('popup_opened');
-}
-
-editButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
-likeButton.addEventListener('click', toggleLike);
-
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close-button');
+let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_about');
-
+let nameInput = document.querySelector('.popup__input_data_name');
+let jobInput = document.querySelector('.popup__input_data_about');
 let profileName = document.querySelector('.profile__title');
 let profileAbout = document.querySelector('.profile__subtitle');
-nameInput.value = profileName.textContent;
-jobInput.value =  profileAbout.textContent;
+
+function togglePopup () {
+  nameInput.value = profileName.textContent;
+  jobInput.value =  profileAbout.textContent;
+  popup.classList.toggle('popup_opened');
+}
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = jobInput.value;
+  togglePopup();
 }
 
+profileEditButton.addEventListener('click', togglePopup);
+popupCloseButton.addEventListener('click', togglePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-saveButton.addEventListener('click', togglePopup);
-
-
-
