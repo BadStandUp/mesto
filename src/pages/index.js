044c40542b24a09f772a9e780d3c51a09046e0d1
BadStandUp <sackboysema@yandex.ru>
2023-01-03 22:32:48 +0500

    feat: fixed card edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ profileEditPopup.setEventListeners();
 const popupAddCard = new PopupWithForm({
   popupSelector: '.popup_add-card',
   callback: data => {
-    const item = {name: data.nameInput, link: data.aboutInput};
+    const item = {name: data.nameInput, link: data.urlInput};
 
     const card = new Card(item, '#card-template', () => handleCardClick(item));
 
